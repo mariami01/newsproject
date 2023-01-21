15427cc0dd3853ae66ed483d4b59687c044ce81f
Mariam Zenaishvili <mariamz@theneo.io>
2023-01-21 17:43:28 +0400

    resgitration and login changes

--- newsproject/urls.py
+++ newsproject/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('about', views.about, name='about'),
     path('admin/', admin.site.urls),
     path('register', views.register, name='register'),
-    path('login', views.login, name='login')
+    path('login', views.login, name='login'),
+    path('logout', views.logout, name='logout'),
 ]
